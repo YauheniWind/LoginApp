46327ae1be9bace67172293dced634f7b85218ba
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2022-11-04 12:35:06 +0100

    1. Setup UI, 2. Made logic

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Евгений  Гравдин  on 04/11/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  
+  @IBOutlet weak var loginTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+    
+    if loginTextField.text == "User" && passwordTextField.text == "1234" {
+      
+      welcomeVC.welcomeValue = loginTextField.text
+      
+    } else {
+      
+      showAlert(alertText: "Someting is wrong", alertMessage: "Login or password is not correct")
+      
+    }
+  }
+  
+  @IBAction func unwind(segue: UIStoryboardSegue) {
+    loginTextField.text = ""
+    passwordTextField.text = ""
+  }
+
+  @IBAction func forgotUserNameButtonPressed() {
+    showAlert(alertText: "Ah you forgot login?", alertMessage: "Login is User")
+  }
+  
+  @IBAction func forgotPasswordButtonPressed() {
+    showAlert(alertText: "You forgot password?", alertMessage: "Password is 1234")
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    self.view.endEditing(true)
+  }
+  
+}
+
+
+extension UIViewController {
+// Show a basic alert
+    func showAlert(alertText: String, alertMessage: String) {
+        let alert = UIAlertController(title: alertText,
+                                      message: alertMessage,
+                                      preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Got it", style: UIAlertAction.Style.default, handler: nil))
+    self.present(alert, animated: true, completion: nil)
+    }
+}
+
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Евгений  Гравдин  on 04/11/2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+  @IBOutlet weak var welcomeLabel: UILabel!
+  
+  var welcomeValue: String!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    welcomeLabel.text = "Welcome, \(welcomeValue ?? "")!"
+    
+    }
+  
+}
